ec33aa15ccc2fd5cdaf87561c9edc45b36fb281c
Juan Chavez <juan@Juans-Air-12.home>
2021-11-25 10:24:19 -0800

    enhanced the add task feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
-var buttonEL = document.querySelector("#save-task");
+var formEL = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(){
-    var taskItemEl = document.createElement("li");
-    taskItemEl.textContent = "new task";
-    taskItemEl.className = "task-item";
-    tasksToDoEl.appendChild(taskItemEl);
-}
+var createTaskHandler = function(event){
+    event.preventDefault();
 
-buttonEL.addEventListener("click", createTaskHandler);
+    var listItemEl = document.createElement("li");
+    listItemEl.textContent = "new task";
+    listItemEl.className = "task-item";
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+formEL.addEventListener("click", createTaskHandler);
 
 
 
