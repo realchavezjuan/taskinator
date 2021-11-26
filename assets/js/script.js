ff8ae9ea1e35d608513c6f509a3a7747df6d4db5
Juan Chavez <juan@Juans-Air-12.home>
2021-11-25 16:11:00 -0800

    completed task to do section

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,27 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event){
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    //creates list item
     var listItemEl = document.createElement("li");
-    listItemEl.textContent = "new task";
     listItemEl.className = "task-item";
+
+    //create div to hold task info
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = 'task-info';
+    //adds HTML content into div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    //add div into a list item
+    listItemEl.appendChild(taskInfoEl);
+    //add list item to entire list
     tasksToDoEl.appendChild(listItemEl);
+
 };
 
-formEL.addEventListener("click", createTaskHandler);
+formEL.addEventListener("submit", createTaskHandler);
 
 
 
